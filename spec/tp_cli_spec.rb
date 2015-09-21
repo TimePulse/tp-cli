cc80a9613ce552773496d3e84c17003d4b3d73c8
Anne Vetto <vettoap@gmail.com>
2015-09-21 15:46:48 -0700

    Begin writing specs, include license

--- spec/tp_cli_spec.rb
+++ spec/tp_cli_spec.rb
@@ -1,1 +1,21 @@
 require 'spec_helper'
+
+describe TpCommandLine::ActivityTrack do
+  subject { TpCommandLine::ActivityTrack.new }
+
+  describe '#det_activity' do
+
+    # let(:input) { 'bin/tp_cli note ok' }
+    # let(:output) { subject.det_activity }
+
+    # it 'checks for cl arguments' do
+    #   expect(output).to eq output
+
+    let(:input) { 'bin/tp_cli' }
+    let(:output) { subject.det_activity }
+
+    it 'checks for cl arguments' do
+      expect(output).to match "Please specificy action: bin/tp_cli [note] or bin/tp_cli [cwd]"
+    end
+  end
+end
