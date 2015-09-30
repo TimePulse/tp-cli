fb85da4dfcfba78666445cd18c49c7863d8c0df9
Anne Vetto <vettoap@gmail.com>
2015-09-30 14:53:07 -0700

    Refactor loading files and configurations for typhoeus request.

--- lib/tp_cli.rb
+++ lib/tp_cli.rb
@@ -22,45 +22,47 @@ module TpCommandLine
       end
     end
 
-    #POST to create method in TP Activities controller; create an Activity in db
-    def record_activity_in_TP(description)
-      home_config = File.join(Dir.home, ".timepulse.yml")
-
-      hash1 = YAML.load(File.read(home_config))
-      hash2 = YAML.load(File.read('config/.timepulse.yml'))
-
-      if File.exists?(home_config) == false
-        hash1 = {}
+    def load_file(file)
+      if File.exists?(file)
+        return YAML.load(File.read(file))
+      else
+        file = {}
       end
-      if File.exists?('config/.timepulse.yml') == false
-        hash2 = {}
-      end
-      if File.exists?(home_config) && File.exists?('config/.timepulse.yml')
-        config_hash = hash1.merge(hash2)
+    end
+
+    def load_config
+      hash1 = load_file(File.join(Dir.home, ".timepulse.yml"))
+      hash2 = load_file('config/.timepulse.yml')
+
+      config_hash = hash1.merge(hash2)
 
       missing_fields = ['timepulse_url', 'project_id', 'login', 'authorization'].find_all {|k| !config_hash.keys.include? k}
-      if missing_fields.empty?
-        url = config_hash['timepulse_url']
-        project_id = config_hash['project_id']
-        login = config_hash['login']
-        authorization = config_hash['authorization']
-      else
-        puts "Missing necessary parameter/s: #{missing_fields.join(", ")}"
+      unless missing_fields.empty?
+        exit "Missing necessary parameter/s: #{missing_fields.join(", ")}"
       end
 
+      return config_hash
+    end
+
+    #POST to create method in TP Activities controller; create an Activity in db
+    def record_activity_in_TP(description)
+      config = load_config
       request = Typhoeus::Request.new(
-        url,
+        config['timepulse_url'],
         method: :post,
         params: { activity: {
           description: description,
-          project_id: project_id,
+          project_id: config['project_id'],
           source: "API"
           }
         },
-        headers: { login: login, Authorization: authorization }
+        headers: { login: config['login'], Authorization: config['authorization'] }
       )
 
-      request.run
+      begin
+        request.run
+      rescue StandardError => err
+        puts "\n Please check your internet connection and that the project site is not currently offline."
       end
     end
   end
